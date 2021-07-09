2e8fd82d0f0be8479653f0d39508280adae5af63
Toby Mahnert <toby.mahnert@rocketboots.com>
2021-07-09 20:25:59 +1000

    added user sagas and code refactoring

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
@@ -45,8 +43,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser 
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
